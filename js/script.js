c50e7942e9a6350072ac74c4bf56f73548804ac9
robertvlad <vladrob96@gmail.com>
2022-12-14 16:29:03 +0100

    Esercizio base + bonus + supervonus 1 e 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
-//Bonus creare elemtenti di dificoltà
+function create_bombs_array(min,max){
+
+    let bombs = []
+
+    let i = 0
+
+    while(i < 16){
+        let number = Math.floor(Math.random() * (max - min + 1)) + min
+
+        if(!bombs.includes(number)){
+            bombs.push(number)
+            i++
+        }
+    }
+
+    return bombs
+
+}
 
 function new_game(){
     let difficulty = parseInt(document.getElementById("levels").value)
@@ -25,9 +42,46 @@ function new_game(){
             break;        
     }
 
-    create_grid(box_num, box_in_a_row)
+    let bombs_array = create_bombs_array(1, box_num)
+    console.log(bombs_array)
+
+    create_grid(box_num, box_in_a_row, bombs_array)
 }
 
+function create_grid(box_num, box_in_a_row, bombs_array){
+
+    document.querySelector(".container").innerHTML = ""
+
+    const grid = document.createElement("div")
+    grid.classList.add("grid")
+
+    let lucky_box = 0
+
+    for(let i=0; i<box_num; i++){
+
+        const box = create_single_box(i+1, box_in_a_row)
+
+        box.addEventListener("click", function(){
+            this.classList.add("clicked")
+            
+            if(bombs_array.includes(parseInt(this.innerText))){
+                this.classList.add("red")
+                grid.classList.add("event-null")
+                alert(`Hai selezionato una bomba: `+this.innerText)
+                alert(`Hai indovinato ${lucky_box} caselle`)
+            }
+            else{
+                lucky_box++         
+            }
+        })
+
+        grid.appendChild(box)
+
+    }
+
+    document.querySelector(".container").appendChild(grid)
+
+}
 
 function create_single_box(num, box_in_a_row){
 
@@ -43,29 +97,6 @@ function create_single_box(num, box_in_a_row){
     box.innerText = num
 
     return box
-
-}
-
-function create_grid(box_num, box_in_a_row){
-
-    document.querySelector(".container").innerHTML = ""
-
-    const grid = document.createElement("div")
-    grid.classList.add("grid")
-
-    for(let i=0; i<box_num; i++){
-
-        const box = create_single_box(i+1, box_in_a_row)
-
-        box.addEventListener("click", function(){
-            this.classList.toggle("clicked")
-        })
-
-        grid.appendChild(box)
-
-    }
-
-    document.querySelector(".container").appendChild(grid)
 
 }
 
